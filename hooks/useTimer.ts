a471ac385df9ab2c0aace4557e06a9807162d7ed
dmm22 <davidmartin71090@gmail.com>
2022-12-14 19:35:23 -0500

    Add useTimer hook

---
+++ hooks/useTimer.ts
@@ -0,0 +1,37 @@
+import { useState, useRef, useEffect, useCallback } from "react"
+
+type TimerDirection = "backward" | "forward"
+
+const useTimer = (startingTime: number, direction: TimerDirection) => {
+  const timerIdRef = useRef<NodeJS.Timer | null>(null)
+  const [timer, setTimer] = useState(startingTime)
+
+  const start = useCallback(() => {
+    if (timerIdRef.current) return
+    timerIdRef.current = setInterval(
+      () =>
+        setTimer(timer => {
+          if (direction === "forward") return timer + 1
+          else return timer - 1
+        }),
+      1000
+    )
+  }, [startingTime, direction])
+
+  const pause = useCallback(() => {
+    timerIdRef.current && clearInterval(timerIdRef.current)
+    timerIdRef.current = null
+  }, [])
+
+  const reset = useCallback(() => {
+    setTimer(startingTime)
+  }, [startingTime])
+
+  useEffect(() => {
+    return () => clearInterval(timerIdRef.current!)
+  }, [])
+
+  return [timer, start, pause, reset] as const
+}
+
+export default useTimer
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,20 @@
 import Text from "../components/Text"
+import useTimer from "../hooks/useTimer"
 import useTypingContext from "../hooks/useTypingContext"
 import useGuidedText from "../hooks/useTypingText"
 
 export default function Home() {
-  const { text, newText, input, handleInput } = useTypingContext()
+  // const { text, newText, input, handleInput } = useTypingContext()
+  const [timer, start, pause, reset] = useTimer(30, "backward")
 
   return (
     <>
-      <input value={input} onChange={e => handleInput(e.target.value)} />
-      <Text />
+      <button onClick={start}>start</button>
+      <button onClick={pause}>pause</button>
+      <button onClick={reset}>reset</button>
+      {timer}
+      {/* <input value={input} onChange={e => handleInput(e.target.value)} />
+      <Text /> */}
     </>
   )
 }
